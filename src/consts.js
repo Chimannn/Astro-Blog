464694c37eee0ebc1afa81ec0b629ebf59fe34f6
Chimannn <1164617068@qq.com>
2023-03-17 16:14:28 +0800

    feat:我的第一次提交

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_TITLE = `Chiman's Blog`;
+export const SITE_DESCRIPTION = 'Sharing in chiman`s life.';
+export const SITE_EMAIL = '1164617068@qq.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
